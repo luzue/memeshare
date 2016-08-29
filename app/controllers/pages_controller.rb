97f4036920b644629c4dc189ec7971f60d3d998c
Tyler <mr.man98@icloud.com>
2016-08-29 23:48:10 +0000

    users can post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,24 @@ class PagesController < ApplicationController
   end
 
   def home
+    
+    @posts = Post.all
   end
 
   def profile
+    
     if (User.find_by_username(params[:id]))
       @username = params[:id]
     else
       redirect_to root_path, :notice=> 'User not found.'
     end
+    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   def explore
+    
+    @posts = Post.all
   end
 end
